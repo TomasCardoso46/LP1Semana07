d54d0df05746f5c2e0eed44e0ad7234681806ad8
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-04-18 12:21:51 +0100

    added the required changes to MilitaryUnit

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -1,14 +1,34 @@
 namespace GameUnits
 {
-    public class MilitaryUnit : Unit // Heranca!!
+    public class MilitaryUnit : Unit // Herança!!
     {
         public int AttackPower { get; }
         public int XP { get; private set; }
 
         public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
-        {                                                        // Unit(int, int)
+        {
             AttackPower = attackPower;
             XP = 0;
         }
+
+        public override int Health // Override da propriedade Health
+        {
+            get { return base.Health + XP; }
+            set { base.Health = value; }
+        }
+
+        public override float Cost // Override da propriedade Cost
+        {
+            get { return AttackPower + XP; }
+        }
+
+        public void Attack(Unit u) // Método Attack
+        {
+            XP++; // Incrementa XP da própria unidade
+
+            // Aplica dano igual a AttackPower na Health da unidade atacada
+            u.Health -= AttackPower;
+        }
     }
 }
+
